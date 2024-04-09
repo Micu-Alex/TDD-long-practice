b1e3a7dd63905c398d8492163f345b9614428873
Micu-Alex <alexmicu389@gmail.com>
2024-04-09 20:41:07 +0300

    done map

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,18 @@
 function myMap(inputArray, callback) {
   // Your code here
+  if (!Array.isArray(inputArray)) {
+    throw new Error("Input must be an array");
+  }
+  if (typeof callback !== 'function') {
+    throw new Error("Callback must be a function");
+  }
+
+  const resultArray = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    resultArray.push(callback(inputArray[i]));
+  }
+  return resultArray;
+
 }
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,35 @@
-// Your code here
+const { expect } = require('chai');
+const spies = require('chai-spies');
+
+const myMap = require('../problems/my-map');
+
+
+describe("myMap", () => {
+    context("invalid arguments", ()=> {
+        it("should throw if inputArray is not an array", () => {
+            expect(() => myMap(0, () => {})).to.throw();
+            expect(() => myMap("test", () => {})).to.throw();
+        }) 
+    
+        it("should throw if callback is not a function", () => {
+            expect(() => myMap([1, 2, 3], 0)).to.throw();
+            expect(() => myMap([1, 2, 3], "test")).to.throw();
+        })
+    })
+
+    context("valid arguments", ()=> {
+        it("should call the callback on each element of the array", () => {
+            const callback = (el) => el * 2;
+            expect(myMap([1, 2, 3], callback)).to.deep.equal([2, 4, 6])
+        })
+      
+
+        it("should not mutate the original argument array", () => {
+            const arr = [1, 2, 3];
+            const callback = (el) => el * 2;
+            const result = myMap(arr, callback);
+            expect(result).to.not.equal(arr);
+            expect(arr).to.deep.equal([1, 2, 3]);
+        });
+     })
+})
